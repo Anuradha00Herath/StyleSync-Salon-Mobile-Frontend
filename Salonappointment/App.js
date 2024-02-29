7ab14062ca82fe00fc7cb9ce53bee6a5f7eaa0b3
Anuradha00Herath <herathanuradha03@gmail.com>
2024-02-29 23:09:02 +0530

    Add image

---
+++ Salonappointment/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SalonAppointmentReceiveHome } from './Screens/salon-appointment-receive-home';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <SalonAppointmentReceiveHome/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Salonappointment/Screens/salon-appointment-receive-home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, ImageBackground } from "react-native";
+
+export function SalonAppointmentReceiveHome(){
+    // return(
+    //     // <ImageBackground source={}></ImageBackground>
+    // )
+}
+
+
