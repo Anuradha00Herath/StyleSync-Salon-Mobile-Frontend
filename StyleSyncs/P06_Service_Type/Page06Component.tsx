f12bae5c2900141e92d55d2db5dcf3fb7e307499
Asangika <asangikanethmini@gmail.com>
2024-04-17 21:55:27 +0530

    edit page)6Component

--- StyleSyncs/P06_Service_Type/Page06Component.tsx
+++ StyleSyncs/P06_Service_Type/Page06Component.tsx
@@ -5,48 +5,45 @@ import { SeparatorLineWithText } from '../Component/line';
 export  function Page06Component({text,price,duration,onPress}) {
     return (
         <View>
-              <View style={styles.container}>
-                <View style={styles.FlexBox1}>
-                      <Text style={styles.text}>{text}</Text>
-                      <Text style={styles.text1}>{duration}Min</Text>
+              <View style={{flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',}}>
+                <View style={{ flex:1,
+                               flexDirection:"column",}}>
+                      <Text style={{fontSize:14,}}>{text}</Text>
+                      <Text style={{fontSize:10,}}>{duration}Min</Text>
                 </View>
-                <View style={styles.FlexBox2}>
-                      <Text style={styles.text}>LKR{price}</Text>
-                </View>
-                <View style={styles.FlexBox3}>
-                      <Icon name='right' size={20} color={"black"} style={{width:20,height:20}}onPress={onPress}/>
+                <View style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                            justifyContent: "flex-start",
+                }}>
+                  <View>
+                     <Text style={{fontSize:14,}}>LKR{price}</Text>
+                  </View>
+                  <View style={{ width: 30, alignItems: "center" }}>
+                      <Icon
+                        name="delete"
+                        size={20}
+                        color={"#71797E"}
+                        style={{ width: 20, height: 20 }}
+                        onPress={onPress}
+                      />
+                  </View>
+                  <View style={{ width: 30, alignItems: "center" }}>
+                      <Icon
+                        name="right"
+                        size={20}
+                        color={"#71797E"}
+                        style={{ width: 20, height: 20 }}
+                        onPress={onPress}
+                      />
+                  </View>
+
                 </View>
               </View>
                 <SeparatorLineWithText lineColor={"gray"}/>
         </View>
     );}
 
-    const styles = StyleSheet.create({
-        container: {
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        },
-        FlexBox1:{
-          flex:1,
-          flexDirection:"column",
-          },
-        text: {
-          fontSize:14,
-        },
-        text1:{
-          fontSize:10,
-          },
-        FlexBox2:{
-          flexGrow:1,
-          alignItems:"flex-end"
-        },
-        FlexBox3:{
-          flex:0.2,
-          width:50,
-          alignItems:"center",
-          //alignItems:"flex-end"
-          
-        },
-    }
-    );
+    
