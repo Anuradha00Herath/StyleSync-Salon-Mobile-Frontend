cebb458a5908ce89488c153b8d3a8942c5322d52
Asangika <asangikanethmini@gmail.com>
2024-04-18 14:23:06 +0530

    change StaffComponant

--- StyleSyncs/P07_Add_Staff/StaffComponant.tsx
+++ StyleSyncs/P07_Add_Staff/StaffComponant.tsx
@@ -5,18 +5,45 @@ import { SeparatorLineWithText } from '../Component/line';
 export  function StaffComponent({name,Service,onPress}){
     return(
         <View>
-             <View style={styles.container}>
-                <View style={styles.FlexBox1}>
-                <View style={styles.Box}></View> 
-                <View style={{ marginLeft:7}}>
-                    <Text>{name}</Text>
+             <View style={{ flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: "space-between",}}>
+                <View style={{flexDirection: 'row',
+                              alignItems: 'center',
+                              justifyContent:"flex-start"}}>
+                    <View style={{width:30,
+                                  height:30,
+                                  backgroundColor:"#D9D9D9",
+                                  borderRadius:5,}}>
+                    </View> 
+                    <View style={{ marginLeft:7}}>
+                        <Text>{name}</Text>
+                    </View>
                 </View>
-                </View>
-                <View style={styles.FlexBox2}>
+                <View style={{ flexDirection: "row",
+                               alignItems: "center",
+                               justifyContent: "flex-start",}}>
+                <View>
                     <Text>{Service}</Text>
                 </View>
-                <View style={styles.FlexBox3}>
-                      <Icon name='right' size={20} color={"black"} style={{width:20,height:20}}onPress={onPress}/>
+                <View style={{ width: 30, alignItems: "center" }}>
+                    <Icon
+                      name="delete"
+                      size={20}
+                      color={"#71797E"}
+                      style={{ width: 20, height: 20 }}
+                      onPress={onPress}
+                />
+                </View>
+                <View style={{ width: 40, alignItems: "center" }}>
+              <Icon
+                name="right"
+                size={20}
+                color={"#71797E"}
+                style={{ width: 20, height: 20 }}
+                onPress={onPress}
+              />
+            </View>
                 </View>
              </View>
              <SeparatorLineWithText lineColor={"gray"}/>
@@ -24,31 +51,3 @@ export  function StaffComponent({name,Service,onPress}){
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: "space-between",
-      },
-    FlexBox1:{
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    Box:{
-        width:30,
-        height:30,
-        backgroundColor:"#D9D9D9",
-        borderRadius:5,
-    },
-    FlexBox2:{
-        flexGrow:1,
-        alignItems:"flex-end"
-      },
-    FlexBox3:{
-        flex:0.2,
-        width:50,
-        alignItems:"center",
-        //alignItems:"flex-end"
-        
-      },
-})
