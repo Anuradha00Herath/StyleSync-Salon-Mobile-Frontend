1ed2135375322173c540dc7e122b5055220bd9a9
Anuradha00Herath <herathanuradha03@gmail.com>
2024-06-28 01:05:54 +0530

    check changes

---
+++ StyleSync-Main/src/features/SalonRegister/screens/AddPhoto/add-salon-image.tsx
@@ -0,0 +1,269 @@
+// import Ionicons from "@expo/vector-icons/Ionicons";
+// import React, { useEffect, useState } from "react";
+// import {
+//   View,
+//   Text,
+//   Image,
+//   KeyboardAvoidingView,
+//   ScrollView,
+//   Pressable,
+//   Platform,
+//   GestureResponderEvent,
+//   StyleSheet,
+//   Button,
+//   TouchableOpacity
+// } from "react-native";
+// import * as ImagePicker from 'expo-image-picker';
+// import axios from "axios";
+
+// export default function EditSalonProfile({ navigation ,route}) {
+//   const {salonId} = route.params;
+//   const [refresh, setRefresh] = useState(false);
+//   const [details, setDetails] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const [name, setName]       =useState("");
+//   const [contactNo, setContactNo] =useState("");
+//   const [error, setError] = useState("");
+//   const [gender, setGender]=useState("All")
+
+//   const fetchDetails = async() =>{
+//     try{
+//       setLoading(true);
+//       const url="https://stylesync-backend-test.onrender.com/app/v1/SalonProfile/get_salon-profileDetails";
+//       console.log('Request parameters',{salonId: salonId, });
+//       const response = await axios.get(url, {params:{salonId: salonId}});
+//       const addressData =response.data.data[0];
+//       setDetails(addressData);
+//       setName(addressData.name);
+//       setContactNo(addressData.contactNo);
+//       console.log(response.data)
+
+//     }catch(error){
+//       console.error(error);
+//     }finally{
+//       setLoading(false);
+//     }
+//   };
+
+//   const validateInputs = () => {
+//     let isValid = true;
+
+//     if (!name.trim()) {
+//       setError("*Name field is required");
+//       isValid = false;
+//       alert("Name field is required");
+//     } else {
+//       setError("");
+//     }
+
+//     if (!gender.trim()) {
+//       setError("*Gender field is required");
+//       isValid = false;
+//       alert("Gender field is required");
+//     } else {
+//       setError("");
+//     }
+
+//     if (!contactNo.trim()) {
+//       setError("*Conatct field is required");
+//       isValid = false;
+//       alert("Conatct field is required");
+//     } else if (!/^(\+94)[0-9]{9}$/.test(contactNo)) {
+//       setError("*Number Should be in correct format");
+//       isValid = false;
+//       alert("Number Should be in correct format");
+//     } else {
+//       setError("");
+//     }
+//     return isValid;
+//   };
+
+//   const handleSubmit = async () => {
+//     if (validateInputs()){
+//     try {
+//       setLoading(true);
+//       const url =
+//         "https://stylesync-backend-test.onrender.com/app/v1/SalonProfile/Update_salon-profile";
+//       const response = await axios.put(url, {
+//         salonId: salonId,
+//         name,
+//         contactNo
+//       });
+
+//      console.log(response.data);
+//      navigation.goBack()
+//     } catch (error) {
+//       console.log(error);
+//     } finally {
+//       setLoading(false);
+//     }
+//     }else {
+//       console.log({ error });
+//     }
+ 
+// };
+
+//   useEffect(() => {
+//     fetchDetails();
+//   }, []);
+
+
+
+//     const [image, setImage] = useState(require("../../../../../../../assets/images.jpg"));
+//     useEffect(() => {
+//         (async () => {
+//           if (Platform.OS !== 'android') {
+//             const libraryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+//             if (libraryStatus.status !== 'granted') {
+//               alert('Sorry, we need camera roll permissions to make this work!');
+//             }
+    
+//             const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+//             if (cameraStatus.status !== 'granted') {
+//               alert('Sorry, we need camera permissions to make this work!');
+//             }
+//           }
+//         })();
+//       }, []);
+
+//       const pickImage = async () => {
+//         let result = await ImagePicker.launchImageLibraryAsync({
+//           mediaTypes: ImagePicker.MediaTypeOptions.All,
+//           allowsEditing: true,
+//           aspect: [4, 3],
+//           quality: 1,
+//         });
+    
+//         if (!result.canceled) {
+//           setImage(result);
+//         }
+//       };
+
+
+//   return (
+//     <View>
+//       <View
+//         style={{
+//           flexDirection: "row",
+//           justifyContent: "space-between",
+//           marginLeft: 10,
+//           marginTop: 10,
+//           marginRight: 10,
+//         }}
+//       >
+//         <Ionicons
+//           style={{
+//             marginTop: 5,
+//           }}
+//           name="arrow-back-outline"
+//           size={22}
+//           color="black"
+//           onPress={() => navigation.goBack()}
+//         />
+//         <Text
+//           style={{
+//             fontSize: 22,
+//             fontWeight: "bold",
+//           }}
+//         >
+//           Edit Salon Profile
+//         </Text>
+//       </View>
+//       <View
+//         style={{
+//           flexDirection: "row",
+//           justifyContent: "center",
+//           marginBottom: 30
+//         }}
+//       >
+//         <Image
+//           source={image}
+//           style={{
+//             width: 150,
+//             height: 150,
+//             marginTop: 50,
+//             alignSelf: "center",
+//             borderRadius: 150,
+//           }}
+//         ></Image>
+//         <View
+//           style={{
+//             marginTop: 165,
+//             marginLeft: -40,
+//             backgroundColor: "rgba(0,0,0,0.2)",
+//             padding: 5,
+//             height: "auto",
+//             borderRadius: 50,
+//           }}
+//         >
+//           <Ionicons
+//             name="camera-outline"
+//             size={25}
+//             color="white"
+//             onPress={pickImage}
+//           />
+//         </View>
+//       </View>
+
+//       <KeyboardAvoidingView behavior="height">
+//         <ScrollView
+//           style={{
+//             alignSelf: "center",
+//           }}
+//         >
+//           <View
+//           style={{
+//             flexDirection: "row",
+//             justifyContent: "space-between",
+//             marginTop: 20,
+//           }}
+//         >
+//           <View
+//             style={{
+//               width: "40%",
+//             }}
+//           >
+//             <TouchableOpacity onPress={()=> navigation.goBack()}>
+//               <View
+//                 style={{
+//                   backgroundColor: "#F5F5F5",
+//                   borderRadius: 10,
+//                   borderWidth: 1,
+//                   padding: 9,
+//                   alignItems: "center",
+//                 }}
+//               >
+//                 <Text>Cancel</Text>
+//               </View>
+//             </TouchableOpacity>
+//           </View>
+//           <View
+//             style={{
+//               width: "40%",
+//             }}
+//           >
+//             <TouchableOpacity onPress={handleSubmit}>
+//               <View
+//                 style={{
+//                   backgroundColor: "#8B6C58",
+//                   borderRadius: 10,
+//                   padding: 10,
+//                   alignItems: "center",
+//                 }}
+//               >
+//                 <Text
+//                   style={{
+//                     color: "white",
+//                   }}
+//                 >
+//                   Save
+//                 </Text>
+//               </View>
+//             </TouchableOpacity>
+//           </View>
+//         </View>
+//         </ScrollView>
+//       </KeyboardAvoidingView>
+//     </View>
+//   );
+// }
