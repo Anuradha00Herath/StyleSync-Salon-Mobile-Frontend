e5da8ea92497e68db4558e91cd5fe71cf74301a2
Anuradha00Herath <herathanuradha03@gmail.com>
2024-07-10 01:28:13 +0530

    add email verified

--- StyleSync-Main/App.js
+++ StyleSync-Main/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { CardStyleInterpolators,createStackNavigator} from '@react-navigation/stack';
 import StaffRegistorNavigation from "./src/navigations/staffRegistorNavigation";
@@ -6,10 +6,14 @@ import ShowAppoinments from './src/navigations/showAppoinmentNavigation';
 import  SalonRegistorNavigation from "./src/navigations/salonRegisterNavigation";
 import PersanalInformation from "./src/features/staff-register/screens/PersonalInformation/PersonalInformation";
 import AddSalonImage from "./src/features/SalonRegister/screens/AddPhoto/add-salon-image";
+import {PermissionsAndroid} from 'react-native';
+
 
 const Stack =createStackNavigator();
 
 export default function App() {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
     return(
         <NavigationContainer>
             <Stack.Navigator initialRouteName="StaffRegistorNavigation" screenOptions={{headerShown: false,}}> 
